874cd64fb67fcd45a87bf113c1a394a89d6277eb
Nahom32 <senaynahom00@gmail.com>
2024-07-12 12:16:18 +0300

    added generate_reduced_permutation from holman's paper

---
+++ Node.py
@@ -0,0 +1,9 @@
+
+class Node:
+    def __init__(self, node_type,val):
+        self.node_type = node_type
+        self.val = val
+        self.left = None
+        self.right = None
+        self.guard_set = []
+    
