585b30cca4519f27443ded485aedf74566f7a238
Yab <yabuman46@gmail.com>
2024-07-17 17:51:28 +0300

    fix: Added new print statement to main

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from Utilities.BuildTree import *
 from Utilities.GatherJunctors import gatherJunctors
-from Utilities.HelperFunctions import print_tree
+from Utilities.HelperFunctions import print_tree, print_constraint_tree
 from Utilities.PropagateTruthValue import propagateTruthValue
-from Utilities.HelperFunctions import isConsistent,union
+
+# from Utilities.HelperFunctions import isConsistent, union
 from DataStructures.Trees import *
-from DataStructures.Graphs import *
-from Utilities.TraverseGraph import *
-#from Utilities.ReduceToElegance import commandSetIterator
-ex_a = BinaryConstraintTreeNode("x")
-ex_b = BinaryConstraintTreeNode("x")
-ex_c = BinaryConstraintTreeNode("y")
-ex_d = BinaryConstraintTreeNode("d")
-ex_e = BinaryConstraintTreeNode("e")
 
-ex_b.constraint = True
+# from DataStructures.Graphs import *
+# from Utilities.TraverseGraph import *
+from Utilities.ReduceToElegance import reduceToElegance
 
-test_data = [ex_a,ex_c,ex_d,ex_b,ex_e]
-test_data2 = [ex_a,ex_c,ex_d,ex_e]
+# ex_a = BinaryConstraintTreeNode("x")
+# ex_b = BinaryConstraintTreeNode("x")
+# ex_c = BinaryConstraintTreeNode("y")
+# ex_d = BinaryConstraintTreeNode("d")
+# ex_e = BinaryConstraintTreeNode("e")
 
-print(isConsistent(test_data))
-print(isConsistent(test_data2))
-print("The consistency is finished")
-print(isConsistent([]))
-print(isConsistent([ex_a]))
+# ex_b.constraint = True
+
+# test_data = [ex_a,ex_c,ex_d,ex_b,ex_e]
+# test_data2 = [ex_a,ex_c,ex_d,ex_e]
+
+# print(isConsistent(test_data))
+# print(isConsistent(test_data2))
+# print("The consistency is finished")
+# print(isConsistent([]))
+# print(isConsistent([ex_a]))
 # g_exa = ConstraintGraphNode()
 # g_exa.value = 'first'
 
@@ -46,7 +49,7 @@ print(isConsistent([ex_a]))
 # print(traverseGraph(g_exa,[],[]))
 # print(union([1,2,3,4,5],[1,3,4,5,8,9]))
 # print(union([],[]))
-#selection_set = []
+# selection_set = []
 # start_node = ConstraintGraphNode()
 # head_and = ConstraintGraphNode()
 # start_node.graphNodeType = GraphNodeType.START
@@ -66,29 +69,29 @@ print(isConsistent([ex_a]))
 
 # traverseGraph(start_node,[],selection_set)
 # print(selection_set)
-node_stop = ConstraintGraphNode()
-node_stop.graphNodeType = GraphNodeType.STOP
+# node_stop = ConstraintGraphNode()
+# node_stop.graphNodeType = GraphNodeType.STOP
 
-node_internal_or = ConstraintGraphNode()
-node_internal_or.graphNodeType = GraphNodeType.INTERNAL
-node_internal_or.type = NodeType.OR
-node_internal_or.children = [node_stop, node_stop]
+# node_internal_or = ConstraintGraphNode()
+# node_internal_or.graphNodeType = GraphNodeType.INTERNAL
+# node_internal_or.type = NodeType.OR
+# node_internal_or.children = [node_stop, node_stop]
 
-node_internal_and = ConstraintGraphNode()
-node_internal_and.graphNodeType = GraphNodeType.INTERNAL
-node_internal_and.type = NodeType.AND
-node_internal_and.next = node_internal_or
-node_internal_and.guardSet = [1, 2]
+# node_internal_and = ConstraintGraphNode()
+# node_internal_and.graphNodeType = GraphNodeType.INTERNAL
+# node_internal_and.type = NodeType.AND
+# node_internal_and.next = node_internal_or
+# node_internal_and.guardSet = [1, 2]
 
-node_start = ConstraintGraphNode()
-node_start.graphNodeType = GraphNodeType.START
-node_start.next = node_internal_and
+# node_start = ConstraintGraphNode()
+# node_start.graphNodeType = GraphNodeType.START
+# node_start.next = node_internal_and
 
-incoming_set = []
-selection_sets = []
+# incoming_set = []
+# selection_sets = []
 
-traverseGraph(node_start, incoming_set, selection_sets)
-print(f"Final selection_sets: {selection_sets}")
+# traverseGraph(node_start, incoming_set, selection_sets)
+# print(f"Final selection_sets: {selection_sets}")
 
 input = "&(B, !(|(C, |(A, &(!(B), A)))))"
 
@@ -110,14 +113,33 @@ constraintTree.type = NodeType.ROOT
 if binaryConstraintTree is not None:
     constraintTree = gatherJunctors(binaryConstraintTree, constraintTree)
 print("Constraint Tree Finished")
+print("Before Reduction")
 
-print("GuardSet: ")
-if constraintTree is not None and constraintTree.guardSet is not None:
-    for gct in constraintTree.guardSet:
-        print_tree(gct)
-    if constraintTree.children:
-        print("Children: ", len(constraintTree.children))
-    
-    if constraintTree.children is not None:
-        for bct in constraintTree.children:
-            print_tree(bct)
+if constraintTree:
+    print_constraint_tree(constraintTree)
+# print("GuardSet: ")
+# if constraintTree is not None and constraintTree.guardSet is not None:
+#     for gct in constraintTree.guardSet:
+#         print_tree(gct)
+#     if constraintTree.children:
+#         print("Children: ", len(constraintTree.children))
+#
+#     if constraintTree.children is not None:
+#         for bct in constraintTree.children:
+#             print_tree(bct)
+
+
+lastAction = reduceToElegance(constraintTree, [], [])
+print("Last action after reduction: ", lastAction)
+if constraintTree:
+    print_constraint_tree(constraintTree)
+# print("GuardSet: ")
+# if constraintTree is not None and constraintTree.guardSet is not None:
+#     for gct in constraintTree.guardSet:
+#         print_tree(gct)
+#     if constraintTree.children:
+#         print("Children: ", len(constraintTree.children))
+#
+#     if constraintTree.children is not None:
+#         for bct in constraintTree.children:
+#             print_tree(bct)
