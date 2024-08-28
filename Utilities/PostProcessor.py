9b0665b28edd8cbafa77bd737229e471230930e1
Nahom32 <senaynahom00@gmail.com>
2024-08-28 09:46:37 +0300

    fix: added an inclusion for guardSet

--- Utilities/PostProcessor.py
+++ Utilities/PostProcessor.py
@@ -1,11 +1,13 @@
 from DataStructures.Trees import *
 def parse_tree_to_lisp(node: TreeNode) -> str:
-    if node.type == NodeType.LITERAL:
+    if node.type == NodeType.LITERAL or (node.type == NodeType.AND and node.children == []):
         return node.value
     elif node.type in {NodeType.AND, NodeType.OR, NodeType.NOT}:
         operator = node.type.value
         children_exprs = [parse_tree_to_lisp(child) for child in node.children]
-        return f"({operator} {' '.join(children_exprs)})"
+        guard_exprs = [parse_tree_to_lisp(guard) for guard in node.guardSet]
+        print(node.guardSet)
+        return f"({operator} {' '.join(children_exprs)}{' '.join(guard_exprs)})"
     elif node.type == NodeType.ROOT:
         return parse_tree_to_lisp(node.children[0]) if node.children else ""
     return ""
