28845551de84e8a64393d99846bed98a7b232634
Yab <yabuman46@gmail.com>
2024-09-03 11:54:15 +0300

    refactor: Made general test for readable
    - Moved enf rule checks to separate function
    - Used for loop to check enf rules instead of manually typing every rule checker function

--- Tests/GeneralTestCases.py
+++ Tests/GeneralTestCases.py
@@ -1,3 +1,4 @@
+import enum
 from unittest import TestCase
 from Tests.TestHelpers import compare_tables, rteRunner
 from Tests.EnfRuleTests import (
@@ -9,6 +10,7 @@ from Tests.EnfRuleTests import (
     ruleSix,
     ruleSeven,
 )
+from Utilities.HelperFunctions import print_constraint_tree
 
 EXPRESSIONS = [
     "&(B, !(|(C, |(A, &(!(B), A)))))",
@@ -42,42 +44,31 @@ EXPRESSIONS = [
 
 
 class GeneralTests(TestCase):
-    def testRTEGeneral(self):
+    def testSemanticMeaning(self):
         for index, input in enumerate(EXPRESSIONS):
             constraintTree, table1, table2 = rteRunner(input)
+
             self.assertTrue(
                 compare_tables(table1, table2),
                 f"Test case {index} failed. {input} changed its semantic meaning after algorithm application.",
             )
 
+    def testEnfRules(self):
+        for index, input in enumerate(EXPRESSIONS):
+            constraintTree, table1, table2 = rteRunner(input)
             if constraintTree:
-                self.assertTrue(
-                    ruleOne(constraintTree),
-                    f"Test case {index} failed. Enf Rule 1 failed.",
-                )
+                enfRules = [
+                    ruleOne,
+                    ruleTwo,
+                    ruleThree,
+                    ruleFour,
+                    ruleFive,
+                    ruleSix,
+                    ruleSeven,
+                ]
 
-                self.assertTrue(
-                    ruleTwo(constraintTree),
-                    f"Test case {index} failed. Enf Rule 2 failed.",
-                )
-
-                self.assertTrue(
-                    ruleThree(constraintTree),
-                    f"Test case {index} failed. Enf Rule 3 failed.",
-                )
-                self.assertTrue(
-                    ruleFour(constraintTree),
-                    f"Test case {index} failed. Enf Rule 4 failed.",
-                )
-                self.assertTrue(
-                    ruleFive(constraintTree),
-                    f"Test case {index} failed. Enf Rule 5 failed.",
-                )
-                self.assertTrue(
-                    ruleSix(constraintTree),
-                    f"Test case {index} failed. Enf Rule 6 failed.",
-                )
-                self.assertTrue(
-                    ruleSeven(constraintTree),
-                    f"Test case {index} failed. Enf Rule 7 failed.",
-                )
+                for ruleIndex, rule in enumerate(enfRules):
+                    self.assertTrue(
+                        rule(constraintTree),
+                        f"Test case {index} failed. Enf Rule {ruleIndex} failed.",
+                    )
