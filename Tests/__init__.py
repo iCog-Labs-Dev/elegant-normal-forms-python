80b85fc69d3ad4982a4a838c2f1028e06d6546ae
Yab <yabuman46@gmail.com>
2024-09-04 09:57:52 +0300

    fix: Import error

--- Tests/__init__.py
+++ Tests/__init__.py
@@ -1,4 +1,4 @@
 from Tests.TransformationTestCases import *
 from Tests.GeneralTestCases import *
 from Tests.TestHelpers import *
-from Tests.EnfRuleTests import *
+from Tests.EnfRuleCheckers import *
